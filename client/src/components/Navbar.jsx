86184de9c49ede60e87fe6596edb553ab504cfef
Mohit Sahu <ee22btech11034@iith.ac.in>
2026-04-16 13:40:56 +0530

    fix mobile responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,17 +14,38 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
       <div className="container-fluid">
-        {/* Brand / Home */}
-        <Link className="navbar-brand" to="/">My Blog</Link>
+        {/* Brand */}
+        <Link className="navbar-brand" to="/">
+          My Blog
+        </Link>
 
-        <div className="collapse navbar-collapse">
+        {/* Mobile Hamburger Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible Menu */}
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
             </li>
+
             {user && (
               <li className="nav-item">
-                <Link className="nav-link" to="/new">Create Post</Link>
+                <Link className="nav-link" to="/new">
+                  Create Post
+                </Link>
               </li>
             )}
           </ul>
@@ -33,10 +54,15 @@ function Navbar() {
             {!user ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">Login</Link>
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
                 </li>
+
                 <li className="nav-item">
-                  <Link className="nav-link" to="/signup">Signup</Link>
+                  <Link className="nav-link" to="/signup">
+                    Signup
+                  </Link>
                 </li>
               </>
             ) : (
@@ -44,6 +70,7 @@ function Navbar() {
                 <li className="nav-item">
                   <span className="navbar-text me-2">Hi, {user.name}</span>
                 </li>
+
                 <li className="nav-item">
                   <button
                     className="btn btn-sm btn-outline-light"
